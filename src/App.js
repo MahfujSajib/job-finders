0d34c83fa93f3fa8a57307d7f0a3748e80166c95
MahfujSajibe <mahfujsajib1998@gmail.comm>
2021-12-07 14:07:17 +0600

    navbar add

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/home" element={<Home />} />
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/findjob" element={<FindJobs />} />
         <Route path="/company" element={<Company />} />
